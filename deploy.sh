c74d311ea9efb1614b1cda28c54d2b4bb7bca48c
Conghui He <heconghui@gmail.com>
2015-11-18 16:07:00 +0800

    add deploy

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+docker run -it \
+           --volume=$(pwd):/srv/jekyll \
+           -w /srv/jekyll \
+           -d \
+           -p 80:4000 \
+           conghui/jekyll:v1 \
+           jekyll serve -H 0.0.0.0
