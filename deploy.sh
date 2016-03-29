fc650b8b2a291f92c371556d318f18c63dbc2f45
Conghui He <heconghui@gmail.com>
2016-03-29 16:25:18 +0800

    change local port to 4000

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ set -x
 docker run -it --rm \
            --volume=$(pwd):/srv/jekyll \
            -w /srv/jekyll \
-           -p 80:4000 \
+           -p 4000:4000 \
            jekyll-3.0 \
            jekyll serve -w -I -H 0.0.0.0
